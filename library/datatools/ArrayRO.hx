64b3295ddb8e3f7450cae5883d1495c7dc6c91f0
Yaroslav Sivakov <yar3333@gmail.com>
2017-05-15 16:55:37 +0300

    * avoid dependency from stdlib

--- library/datatools/ArrayRO.hx
+++ library/datatools/ArrayRO.hx
@@ -9,8 +9,27 @@ abstract ArrayRO<T>(std.Array<T>) from std.Array<T>
 	public inline function exists(f:T->Bool) return Lambda.exists(this, f);
 	public inline function foreach(f:T->Bool) return Lambda.foreach(this, f);
 	
-	public function findIndex(f:T->Bool) return stdlib.Lambda.LambdaIterable.findIndex(this, f);
-	public function findLastIndex(f:T->Bool) return stdlib.Lambda.LambdaIterable.findLastIndex(this, f);
+	public function findIndex(f:T->Bool)
+	{
+		var n = 0;
+		for (item in this)
+		{
+			if (f(item)) return n;
+			n++;
+		}
+		return -1;
+	}
+	
+	public function findLastIndex(f:T->Bool)
+	{
+		var n = this.length - 1;
+		while (n >= 0)
+		{
+			if (f(this[n])) return n;
+			n--;
+		}
+		return -1;
+	}
 	
 	public static function fromDerived<T, Z:T>(arr:ArrayRO<Z>) return (cast arr:ArrayRO<T>);
 }
